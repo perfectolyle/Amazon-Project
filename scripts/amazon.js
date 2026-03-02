3570d8f62d6b6c1abba2930c0e5ade0af393a803
Perfecto <perfectogardoce@gmail.com>
2026-03-02 13:16:58 +0800

    including the scripts in the first page

---
+++ scripts/amazon.js
@@ -0,0 +1,28 @@
+const products = [{
+        image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+        name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+        price: 1090,
+        rating: {
+            stars: 4,
+            count: 87
+        }
+    },
+    {
+        image: "images/products/intermediate-composite-basketball.jpg",
+        name: "Intermediate Size Basketball",
+        price: 2095,
+        rating: {
+            stars: 4,
+            count: 127
+        }
+    },
+    {
+        image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
+        name: "Adults Plain Cotton T-Shirt - 2 Pack",
+        price: 799,
+        rating: {
+            stars: 4,
+            count: 56
+        }
+    }
+];
