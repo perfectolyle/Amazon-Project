89969ca2f0b8344dc519ed6a6197dd4e90c43fb5
Perfecto <perfectogardoce@gmail.com>
2026-03-02 13:43:07 +0800

    fixed the issues with the price tags

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -21,7 +21,7 @@ products.forEach(product => {
             </div>
 
             <div class="product-price">
-                $${(product.price / 100).toFixed(2)}
+                $${(product.priceCents / 100).toFixed(2)}
             </div>
 
             <div class="product-quantity-container">
