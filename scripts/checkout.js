801260326fd0d858b687a4a247380e3d5bfe7a5a
Perfecto <perfectogardoce@gmail.com>
2026-05-22 12:21:29 +0800

    Improve delete order feature

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -3,10 +3,10 @@ import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 
 let cartSummary = '';
+let matchingProduct;
+
 cart.forEach((cartItem, index) => {
     const productId = cartItem.productId;
-
-    let matchingProduct;
 
     products.forEach((product) => {
         if(product.id === productId){
@@ -14,7 +14,7 @@ cart.forEach((cartItem, index) => {
         }
     });
     cartSummary += `
-        <div class="cart-item-container">
+        <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
               Delivery date: Tuesday, June 21
             </div>
@@ -93,6 +93,7 @@ cart.forEach((cartItem, index) => {
 });
 
 document.querySelector('.js-order-summary').innerHTML = cartSummary;
+
 document.querySelectorAll('.js-delete-link')
     .forEach((deleteLink) => {
         deleteLink.addEventListener('click', () => {
@@ -100,6 +101,7 @@ document.querySelectorAll('.js-delete-link')
             
             removeFromCart(productId);
 
-            
+            const container = document.querySelector(`.js-cart-item-container-${productId}`);
+            container.remove()
         });
     });
