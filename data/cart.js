7f34c58eacbf5a6a8c3ca2ca07f9037062ddaa15
Perfecto <perfectogardoce@gmail.com>
2026-05-22 10:28:31 +0800

    moved the add to cart function, and used modules

--- data/cart.js
+++ data/cart.js
@@ -1,3 +1,21 @@
 export const cart = [
     
 ];
+
+export function addToCart(productId){
+  let matchingItem;
+            cart.forEach((item) => {
+                if(productId === item.productId){
+                    matchingItem = item;
+                }
+            });
+
+            if(matchingItem){
+                matchingItem.quantity += 1
+            } else {
+                cart.push({
+                    productId,
+                    quantity: 1
+                });
+            }
+};
